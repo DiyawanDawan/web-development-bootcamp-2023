cac0aa812c59d5dcf1638e1a521fd157174881b6
DiyawanDawan <diyawan0@gmail.com>
2023-08-07 09:22:11 +0800

    Intrpduction ES6

---
+++ 14.0 Naming for Javasrcipt Convert/index.js
@@ -0,0 +1,16 @@
+var name = "awan";
+// var my nme = 'Awan' False
+// not space, not number, not name var let etc
+var fristName = "Diya";
+var gameLevel = 1;
+gameLevel = 2;
+gameLevel = 3;
+// alert('Your Lavels is Currency: ' + gameLevel); 
+console.log(name);
+console.log(name + fristName);
+console.log(`${name} ${fristName}`);
+
+var massage = 'Halo';
+var myfriend = "Anngela";
+// alert(massage + " Welcome to website deading Page "+ myfriend);
+console.log(myfriend.length);
---
+++ 14.2 String Length Retiaving Number of Charakter/index.js
@@ -0,0 +1,7 @@
+var lastName = "Awan";
+console.log(lastName.length);
+
+// Halange 
+var tweet = prompt('Compose your Tweet :');
+var tweetCount = tweet.length;
+alert('You Have written ' + tweetCount + " character, you have " + (140 - tweetCount ) + " characters remaining." )
